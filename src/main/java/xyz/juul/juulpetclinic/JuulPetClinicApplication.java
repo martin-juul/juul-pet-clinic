18bc55ea8a0fb7b078c0d0fcd4b4f85f3a864e13
Martin Juul <code@juul.xyz>
2022-08-17 08:40:33 +0200

    initial commit

---
+++ src/main/java/xyz/juul/juulpetclinic/JuulPetClinicApplication.java
@@ -0,0 +1,13 @@
+package xyz.juul.juulpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuulPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuulPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/juul/juulpetclinic/JuulPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.juul.juulpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuulPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
